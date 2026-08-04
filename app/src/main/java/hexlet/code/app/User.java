4bdcc7f437bef2ce3daca00bf9ba68a929b30215
Nodirbek <126442785+nodirbek9@users.noreply.github.com>
2026-08-04 17:09:52 +0500

    some changes

---
+++ app/src/main/java/hexlet/code/app/User.java
@@ -0,0 +1,30 @@
+package hexlet.code.app;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String firstName;
+
+    String lastName;
+
+    String email;
+
+    String password;
+
+    LocalDate createdAt;
+
+    LocalDate updatedAt;
+}
