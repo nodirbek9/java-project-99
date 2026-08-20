fa8e1aaab2b788efe11217d70fa3c2ce3439952c
Nodirbek <126442785+nodirbek9@users.noreply.github.com>
2026-08-20 11:03:51 +0500

    Validationcontroller fixed

--- src/main/java/hexlet/code/config/ValidationConfig.java
+++ src/main/java/hexlet/code/config/ValidationConfig.java
@@ -2,9 +2,7 @@ package hexlet.code.config;
 
 import hexlet.code.util.JsonNullableValueExtractor;
 import jakarta.validation.Configuration;
-import jakarta.validation.valueextraction.ValueExtractor;
 import org.hibernate.validator.HibernateValidatorConfiguration;
-import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -12,15 +10,16 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @org.springframework.context.annotation.Configuration
 public class ValidationConfig {
 
-    public static class JsonNullableValueExtractor
-            implements ValueExtractor<JsonNullable<?>> {
-
-        @Override
-        public void extractValues(JsonNullable<?> originalValue,
-                                  ValueReceiver receiver) {
-            if (originalValue != null && originalValue.isPresent()) {
-                receiver.value(null, originalValue.get());
+    @Bean
+    @Primary
+    public LocalValidatorFactoryBean defaultValidator() {
+        return new LocalValidatorFactoryBean() {
+            @Override
+            protected void postProcessConfiguration(Configuration<?> configuration) {
+                if (configuration instanceof HibernateValidatorConfiguration hibernateConfiguration) {
+                    hibernateConfiguration.addValueExtractor(new JsonNullableValueExtractor());
+                }
             }
-        }
+        };
     }
 }
